0629042b0c2c2cd86ab64eaa4b917bc846569f2f
Jesper Orb <jesper.orb@zocom.se>
2018-08-27 10:04:14 +0200

    Redux store working

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 import { createStore } from 'redux';
 
+function counter(state = 0, action){
+  switch(action.type){
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(
+  counter,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
+
+console.log(store.getState());
+
+
+
+
+
+
+
+
+
+
+
+
 class App extends Component {
   render() {
     return (
       <div>
-        <p>Heu heu ho</p>
+        
       </div>
     );
   }
